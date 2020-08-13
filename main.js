d459c54b584d6e4a8345a224ae92ec21b9da3d9c
Miracle <51058450+devMiracle@users.noreply.github.com>
2020-08-13 03:59:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ document.oncontextmenu = (e) => {
     return false
 }
 
+var targetFrom = 0
+var targetTo = 0
+
 const viewState = {
     'nodes': [],
     'connections': []
@@ -35,8 +38,10 @@ const drawCanvas = () => {
                 'x': rand(100, 900),
                 'y': rand(100, 900)
             })
+            
             $('canvas').removeLayers()
             drawCanvas()
+           
 
             viewState.connections.forEach(conn => {
                 $('canvas').drawLine({
@@ -48,6 +53,7 @@ const drawCanvas = () => {
                     x2: conn.toNode.x, y2: conn.toNode.y
                 })
             })
+
             viewState.nodes.forEach(node => {
                 $('canvas').drawArc({
                     layer: true,
@@ -72,8 +78,8 @@ const drawCanvas = () => {
                                         x1: currentConnection.fromNode.x, y1: currentConnection.fromNode.y,
                                         x2: currentConnection.toNode.x, y2: currentConnection.toNode.y,
                                     })
-                                    
-                                   
+
+
                                 }
                             } else {
                                 viewState.connections.push({
@@ -81,12 +87,71 @@ const drawCanvas = () => {
                                 })
                             }
                         }
+                        if (ev.event.button == 0) {
+                            for (let j = 0; j < viewState.connections.length; j++) {
+                                if (node.x == viewState.connections[j].fromNode.x & node.y == viewState.connections[j].fromNode.y) {
+                                    targetFrom = viewState.connections[j]
+                                    console.log(node.x + ' ' + viewState.connections[j].fromNode.x + ' ' + node.y + ' ' + viewState.connections[j].fromNode.y)
+                                    break
+                                } else if (node.x == viewState.connections[j].toNode.x & node.y == viewState.connections[j].toNode.y) {
+                                    targetTo = viewState.connections[j]
+                                    console.log(node.x + ' ' + viewState.connections[j].toNode.x + ' ' + node.y + ' ' + viewState.connections[j].toNode.y)
+                                    break
+                                }
+                                else {
+                                    console.log('NO')
+                                }
+                            }
+
+                        }
                     },
-                    drag: function() {
+                    mouseup: function (ev) {
+                        targetFrom = 0
+                        targetTo = 0
+                        
+                    },
+                    drag: function (layer) {
+                        node.x = layer.x
+                        node.y = layer.y
+                        if (targetFrom != 0) {
+                            
+                            targetFrom.fromNode.x = node.x
+                            targetFrom.fromNode.y = node.y
+                            // console.log(targetFrom.fromNode)
+                            
+                            // viewState.connections.forEach(conn => {
+                            //     $('canvas').drawLine({
+                            //         layer: true,
+                            //         strokeStyle: '#000',
+                            //         strokeWidth: 3,
+                            //         bringToFront: false,
+                            //         x1: conn.fromNode.x, y1: conn.fromNode.y,
+                            //         x2: conn.toNode.x, y2: conn.toNode.y
+                            //     })
+                            // })
+                        }
+                        else if (targetTo != 0) {
+                            
+                            targetTo.toNode.x = node.x
+                            targetTo.toNode.y = node.y
+                            // console.log(targetTo.toNode)
+
+                            // viewState.connections.forEach(conn => {
+                            //     $('canvas').drawLine({
+                            //         layer: true,
+                            //         strokeStyle: '#000',
+                            //         strokeWidth: 3,
+                            //         bringToFront: false,
+                            //         x1: conn.fromNode.x, y1: conn.fromNode.y,
+                            //         x2: conn.toNode.x, y2: conn.toNode.y
+                            //     })
+                            // })
+                        }
                        
-                                      
                         
-                        
+            
+                
+
                     },
                     dragstop: function (layer) {
                         node.x = layer.x
